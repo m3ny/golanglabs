10fe99901273785b7c6198c344d2e44a600421b8
john <jrnixon@gmail.com>
2018-11-22 22:06:49 +0100

    new task

---
+++ hashes.go
@@ -0,0 +1,20 @@
+//write  function which counts the number of bits that are differeint in 2 256bit hashes
+package main
+
+//old
+var pc [256]byte
+
+func PopCount(x unit64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func main() {
+
+}
