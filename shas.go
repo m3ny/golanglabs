1bf43995574d784136c827f5ee9ff8c64553c739
john <jrnixon@gmail.com>
2018-11-22 21:59:15 +0100

    issue with args

--- shas.go
+++ shas.go
@@ -2,10 +2,33 @@ package main //Write a program that prints sha256 hash of its standard input by 
 // support command line flag to print sha384 or sha512
 
 import (
+	"bufio"
 	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"os"
 )
 
 func main() {
+	var args string
+	if len(os.Args) > 1 {
+		args := args + os.Args[1]
+	} else {
+		args := ""
+	}
 
-	c1 := sha256.Sum256([]byte("x"))
+	fmt.Println("Enter item to hash:")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	i := input.Text()
+
+	switch args {
+	case "SHA384":
+		fmt.Printf("Result: %x\n", sha512.Sum384([]byte(i)))
+	case "SHA512":
+		fmt.Printf("Result: %x\n", sha512.Sum512([]byte(i)))
+	default:
+		fmt.Printf("Result: %x\n", sha256.Sum256([]byte(i)))
+	}
+	fmt.Println("Exiting")
 }
